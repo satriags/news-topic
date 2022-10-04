6bdfecc98e412ba60c0c0fc82db29850dd272159
Satria Giri Syawalludin <satriagirisyawalludin@gmail.com>
2022-10-05 01:58:03 +0700

    penambahan select status articel

--- services/news-service.ts
+++ services/news-service.ts
@@ -1,7 +1,7 @@
 import { db } from '../connection'
 
 export const getNews = async () => {
-    return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC")
+    return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at,A.status_articel FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC")
 }
 
 
@@ -9,7 +9,7 @@ export const getNews = async () => {
 export const getSearch = async (filter: any) => {
 
     if (filter.keywords == '' && filter.filter_topic == '') {
-        return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC")
+        return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at,A.status_articel FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC")
     } else {
 
 
@@ -24,12 +24,12 @@ export const getSearch = async (filter: any) => {
             filter2 = " A.id_topic = $2 AND  ";
         }
 
-        return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE "
+        return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at,A.status_articel FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE "
             + filter1
             + filter2
             + " A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC", ['%' + filter.keywords + '%', filter.filter_topic, filter.keywords])
 
-        /* return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE "
+        /* return await db.query("SELECT A.id_articel,T.name_topic,A.title_articel,A.slug_articel,A.content_articel,T.id_topic,A.created_at,A.status_articel FROM articel A JOIN topic T ON T.id_topic=A.id_topic WHERE "
         + (filter.keywords == '' ? "" : "A.title_articel LIKE $1 AND ")
         + (filter.filter_topic == '' ? "" : "A.id_topic = $2 AND ")
         + " A.status_articel NOT IN ('deleted') ORDER BY T.id_topic DESC", ['%'+filter.keywords+'%', filter.filter_topic]) */
