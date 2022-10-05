3fb4bb4024ba33ae0a18caa91f4a8a9f9139d40a
Satria Giri Syawalludin <satriagirisyawalludin@gmail.com>
2022-10-06 03:33:24 +0700

    penambahan tabel, nama database pastikan news_topic

---
+++ topic.sql
@@ -0,0 +1,3 @@
+INSERT INTO "topic" ("id_topic", "name_topic", "created_at", "updated_at", "deleted_at") VALUES (36, 'KOMPUTER', '2022-10-06 02:58:32', NULL, '2022-10-06 03:03:42');
+INSERT INTO "topic" ("id_topic", "name_topic", "created_at", "updated_at", "deleted_at") VALUES (35, 'SEPAK BOLA', '2022-10-04 23:22:43', NULL, '2022-10-06 03:03:42');
+INSERT INTO "topic" ("id_topic", "name_topic", "created_at", "updated_at", "deleted_at") VALUES (28, 'WISATA', '2022-10-04 23:13:46', '2022-10-06 02:59:49', '2022-10-06 03:03:42');
